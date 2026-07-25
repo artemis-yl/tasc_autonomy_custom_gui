9cee6cd7c5a0016bed00223378a229de18329934
Artemis Lee <96yrlee@gmail.com>
2026-07-25 01:57:01 -0400

    Add ONVIF credentials to constants.py
    Added ONVIF credentials and updated camera configuration.

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
 JETSON_IP = "192.168.1.7"
+
 IP_RTSP_PORT = 8554
 IP_ONVIF_PORT = 6688
+ONVIF_USERNAME = "admin"
+ONVIF_PASSWORD = ""
+ONVIF_PROFILE = 1
 
 CAMERA_INFO = {
     "Orbbec / Front" : {
@@ -23,13 +27,13 @@ CAMERA_INFO = {
         "place" : "top",
         "ip" : "192.168.1.117",
         "port" : IP_RTSP_PORT,
-        "onvif_port" : IP_ONVIF_PORT
     },            
     "IP Cam 2 / ARM" : {
         "type" : "ip",
         "name" : "arm",   
         "place" : "arm",
-        "ip" : "192.168.1.116", "port" : IP_RTSP_PORT, 
-        "onvif_port" : IP_ONVIF_PORT
+        "ip" : "192.168.1.116", 
+        "port" : IP_RTSP_PORT, 
       }, 
 }
+
